f4e23cb44e3480777aeedd2f1768eb932aa8d1e3
rsakhi <sakhi.rohit@gmail.com>
2019-03-26 12:19:36 +0530

    Create card style

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  state = {
+    person: [{name: "rohit"}]
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
             Learn React
         </header>
+        <div className="person">
+          Rohit Sakhi
+        </div>
       </div>
     );
   }
