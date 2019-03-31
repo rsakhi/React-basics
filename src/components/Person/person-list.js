513fcf595e3bc50b15b874dd4cbf97acc2e86d5a
rsakhi <sakhi.rohit@gmail.com>
2019-03-31 14:32:32 +0530

    Creates HOC component to support Adjacent JSX elements

--- src/components/Person/person-list.js
+++ src/components/Person/person-list.js
@@ -2,20 +2,20 @@ import React, {Component} from 'react';
 import Person from './person'
 class PersonList extends Component {
 
-    shouldComponentUpdate(nextProps){
-        return this.props.persons !== nextProps.persons
-    }
-    
-    render(){
-        return (
-            this.props.persons.map((p,index) => {
-                return <Person name={p.name} age={p.age} key={p.id}
-                cliked={() => this.props.cliked(p.id)}
-                changed={(event) => this.props.updateName(event,p.id)}
-                />
-              })
-        )
-    }
+	shouldComponentUpdate(nextProps){
+		return this.props.persons !== nextProps.persons
+	}
+	
+	render(){
+		return (
+			this.props.persons.map((p,index) => {
+					return <Person name={p.name} age={p.age} key={p.id}
+					cliked={() => this.props.cliked(p.id)}
+					changed={(event) => this.props.updateName(event,p.id)}
+					/>
+			})
+		)
+	}
 }
 
 export default PersonList
--- src/components/Person/person.js
+++ src/components/Person/person.js
@@ -1,15 +1,19 @@
 import React, {useEffect} from 'react';
 import '../../containers/App.css'
+import Aux from '../HOC/Aux'
 
 const person = (props) => {
   useEffect(() => {
     console.log("person")
   },[props.name])
   
-  return (<div className="Person">
+  return <Aux><div className="Person">
     {props.name} | {props.age}<br/>
     <input type="test" onChange={props.changed} value={props.name}/><br/>
     <button onClick={props.cliked}>Delete</button>
-  </div>)
+  </div>
+  <p>
+    Adjacent JSX elements
+  </p></Aux>
 }
 export default person
