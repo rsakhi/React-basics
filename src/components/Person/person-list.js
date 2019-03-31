c76c47146c9839a46871b149e3b74f937a8b854a
rsakhi <sakhi.rohit@gmail.com>
2019-03-31 14:08:02 +0530

    use hook's [shouldComponentUpdate, useEffect] to optimize rendering

--- src/components/Person/person-list.js
+++ src/components/Person/person-list.js
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Person from './person'
-const PersonList = (props) => {
-    return (
-        props.persons.map((p,index) => {
-            return <Person name={p.name} age={p.age} key={p.id}
-            cliked={() => props.cliked(p.id)}
-            changed={(event) => props.updateName(event,p.id)}
-            />
-          })
-    )
+class PersonList extends Component {
+
+    shouldComponentUpdate(nextProps){
+        return this.props.persons !== nextProps.persons
+    }
+    
+    render(){
+        return (
+            this.props.persons.map((p,index) => {
+                return <Person name={p.name} age={p.age} key={p.id}
+                cliked={() => this.props.cliked(p.id)}
+                changed={(event) => this.props.updateName(event,p.id)}
+                />
+              })
+        )
+    }
 }
 
 export default PersonList
--- src/components/Person/person.js
+++ src/components/Person/person.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import '../../containers/App.css'
 
 const person = (props) => {
-        return (<div className="Person">
-        {props.name} | {props.age}<br/>
-        <input type="test" onChange={props.changed} value={props.name}/><br/>
-        <button onClick={props.cliked}>Delete</button>
-      </div>)
+  useEffect(() => {
+    console.log("person")
+  },[props.name])
+  
+  return (<div className="Person">
+    {props.name} | {props.age}<br/>
+    <input type="test" onChange={props.changed} value={props.name}/><br/>
+    <button onClick={props.cliked}>Delete</button>
+  </div>)
 }
-export default person;
+export default person
